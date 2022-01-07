22d0487b0da0f53b14f3dc406f213869df6100a2
matias polhwein <mrmatias892@gmail.com>
2022-01-07 16:36:49 -0300

    pruebas iniciales y revisiones de codigo

---
+++ test/desktop/src/Main/espacio_matiense.java
@@ -0,0 +1,16 @@
+package Main;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class espacio_matiense {
+    public espacio_matiense() {
+        FileHandle archivo = new FileHandle("C:\\Users\\HP\\Documents\\Aegis\\test\\materiales\\pruebainicialpersonaje.png");
+        Texture texture = new Texture(archivo);
+
+        Sprite CubitoDePrueba = new Sprite(texture, 0, 0);
+
+
+    }
+}
--- test/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ test/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,27 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.mygdx.game.MyGdxGame;
+import Main.espacio_matiense;
+
+import java.io.File;
 
 public class DesktopLauncher {
+
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyGdxGame(), config);
+		espacio_matiense ES = new espacio_matiense();
+
+
+
+
+
+
+
 	}
 }
